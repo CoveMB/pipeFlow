f6a9d65b88ea7ef98f8d9e5d4ab342deef4debcc
Benjamin <bmarquiscom@gmail.com>
2021-02-08 18:16:03 -0500

    Add utilities

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,13 +1,12 @@
 import * as R from "ramda";
-import { FlowSyncMiddleware } from "../types";
-import { addToProperty } from "../utils/context";
+import { FlowContext } from "../types";
 
 /**
  * Log the context object without stopping process of the flow
  */
-const debugFlow: (path?: string | string[]) => FlowSyncMiddleware = (path?) => (
-  context
-) =>
+const debugFlow: (
+  path?: string | string[] | number | number[]
+) => (context: FlowContext) => void = (path?) => (context) =>
   console.debug(
     // eslint-disable-next-line array-func/prefer-array-from
     path ? R.path([...(Array.isArray(path) ? path : [path])], context) : context
@@ -16,11 +15,25 @@ const debugFlow: (path?: string | string[]) => FlowSyncMiddleware = (path?) => (
 /**
  * Little helper to merge some data in the actual state property
  */
-const addToReturn = addToProperty("return");
+const addToReturn = R.curry(
+  <M extends Record<any, any> = Record<any, any>>(
+    toReturn: M,
+    context: FlowContext
+  ): FlowContext["return"] & M => {
+    context.return = R.merge(toReturn, context.return);
+
+    return undefined;
+  }
+);
 
 /**
  * Little helper to merge some data in the actual return property
  */
-const addToState = addToProperty("state");
+const addToState = R.curry(
+  <M extends Record<any, any> = Record<any, any>>(
+    toState: M,
+    context: FlowContext
+  ): FlowContext["state"] & M => R.merge(toState, context.state)
+);
 
 export { debugFlow, addToReturn, addToState };
